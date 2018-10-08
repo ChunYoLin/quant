1a1b9a73afb26fb33b5c05876da12d6461af7cf7
Chunyolin <r04922108@csie.ntu.edu.tw>
2018-10-08 22:11:00 +0800

    add: class Fetcher

---
+++ fetcher.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime, timedelta
+
+import pandas as pd
+import pandas_datareader.data as web
+
+
+class Fetcher:
+    def __init__(self):
+        pass
+
+    def fetch(self, stock, start, end=date.today()):
+        data = web.QuandlReader(stock, start, end, api_key="9rtGDsF3JKZLcWAxGbY5").read()
+        data.rename(columns=lambda x: x.lower(), inplace=True)
+        last_date_in_first_fetch = data.index[0].date()
+        if end > data.index[0].date():
+            start_date_in_second_fetch = last_date_in_first_fetch + timedelta(days=1)
+            recent_data = web.DataReader(stock, 'iex', start_date_in_second_fetch, end)
+        data = pd.concat([data, recent_data], axis=0, sort=False)
+        data.index = pd.to_datetime(data.index)
+        return data.sort_index()
+
+fetcher = Fetcher()
+data = fetcher.fetch("AMD", "2000-01-01")
+print(data)
