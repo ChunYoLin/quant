2a8756dbe62681d4d1a7830a8b1c27c688033cf0
Chunyolin <jeff042099@gmail.com>
2018-10-10 17:14:08 +0800

    refactoring: move get_train_datas into StockDataset for the encapsulation

--- test/test_stockdataset.py
+++ test/test_stockdataset.py
@@ -2,7 +2,6 @@ import unittest
 
 from fetcher import Fetcher
 from torch_data import StockDataset
-from torch_data import get_train_datas
 
 class TestStockDataset(unittest.TestCase):
 
@@ -13,13 +12,18 @@ class TestStockDataset(unittest.TestCase):
         data_len = 5
         amd_dataset = StockDataset("AMD", "1993-01-01", "1994-01-05", data_len = data_len)
         amd_df = amd_dataset.data_df
-        self.assertEqual(len(amd_df)-data_len, len(amd_dataset))
+
+        amd_dataset_len = len(amd_dataset)
+
+        self.assertEqual(len(amd_df)-data_len, amd_dataset_len)
 
     def test_get_train_datas(self):
         data_len = 5
-        amd_data_df = StockDataset("AMD", "1993-01-01", "1994-01-05", data_len = data_len).data_df
+        amd_dataset = StockDataset("AMD", "1993-01-01", "1994-01-05", data_len = data_len)
+        amd_data_df = amd_dataset.data_df
         data = amd_data_df.values
 
-        data_x, data_y = get_train_datas(data, data_len)
+        data_x, data_y = amd_dataset.get_train_datas()
+
         self.assertEqual(amd_data_df.iloc[:data_len].values.tolist(), data_x[0].tolist())
         self.assertEqual(amd_data_df.iloc[data_len]["close"], data_y[0])
--- torch_data.py
+++ torch_data.py
@@ -10,18 +10,12 @@ def chunks(data, data_len):
     for idx in range(len(data) - data_len + 1):
         yield data[idx:idx+data_len]
 
-def get_train_datas(data, data_len):
-    data_x = np.array(list(chunks(data, data_len))[:-1])
-    data_y = np.zeros([data_x.shape[0], 1])
-    for idx in range(len(data) - data_len):
-        data_y[idx] = data[idx + data_len, 3]
-    return data_x, data_y
-
 class StockDataset(data.Dataset):
     def __init__(self, symbol, start, end=date.today(), data_len=5):
         usecols = ["open", "high", "low", "close", "volume"]
         self.data_df = Fetcher().fetch(symbol, start, end)[usecols]
-        self.data_x, self.data_y = get_train_datas(self.data_df.values, data_len)
+        self.data_len = data_len
+        self.data_x, self.data_y = self.get_train_datas()
 
     def __getitem__(self, idx):
         return self.data_x, self.data_y
@@ -29,3 +23,11 @@ class StockDataset(data.Dataset):
     def __len__(self):
         return len(self.data_x)
 
+    def get_train_datas(self):
+        data = self.data_df.values
+        data_len = self.data_len
+        data_x = np.array(list(chunks(data, data_len))[:-1])
+        data_y = np.zeros([data_x.shape[0], 1])
+        for idx in range(len(data) - data_len):
+            data_y[idx] = data[idx + data_len, 3]
+        return data_x, data_y
