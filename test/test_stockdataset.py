5f53510e9eb73093cdda4ec937c1c016b81b8775
Chunyolin <jeff042099@gmail.com>
2018-10-10 16:52:42 +0800

    add: test for get_train_datas

--- test/test_stockdataset.py
+++ test/test_stockdataset.py
@@ -1,8 +1,25 @@
 import unittest
-from torch_data import StockDataset
 
+from fetcher import Fetcher
+from torch_data import StockDataset
+from torch_data import get_train_datas
 
 class TestStockDataset(unittest.TestCase):
 
     def test_ctor(self):
         amd_dataset = StockDataset("AMD", "1993-01-01", "1994-01-05", data_len = 5)
+
+    def test_len(self):
+        data_len = 5
+        amd_dataset = StockDataset("AMD", "1993-01-01", "1994-01-05", data_len = data_len)
+        amd_df = amd_dataset.data_df
+        self.assertEqual(len(amd_df)-data_len, len(amd_dataset))
+
+    def test_get_train_datas(self):
+        data_len = 5
+        amd_data_df = StockDataset("AMD", "1993-01-01", "1994-01-05", data_len = data_len).data_df
+        data = amd_data_df.values
+
+        data_x, data_y = get_train_datas(data, data_len)
+        self.assertEqual(amd_data_df.iloc[:data_len].values.tolist(), data_x[0].tolist())
+        self.assertEqual(amd_data_df.iloc[data_len]["close"], data_y[0])
