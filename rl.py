361b9d9288c50de6a800f67d74cc40204de6926e
Chunyolin <r04922108@csie.ntu.edu.tw>
2018-11-14 00:58:29 +0800

    add: btgym as submodule

--- rl.py
+++ rl.py
@@ -5,9 +5,33 @@ from fetcher import Fetcher
 import cv2
 
 
-
-dataset = BTgymDataset(filename='nvda_dataset.csv', parsing_params={"header": 0, "parse_dates": True, "index_col": 0, "sep": ",", "datetime": 0, "open": 1, "high": 2, "low": 3, "close": 4, "volume": 5, "openinterest": -1, "names": [
-                       "open", "high", "low", "close", "volume", "exdividend", "splitratio", "adjopen", "adjhigh", "adjlow", "adjclose", "adjvolume"], "timeframe": 1}, episode_duration={"days": 1, "hours": 0, "minutes": 0}, time_gap={"days": 3000, "hours": 0, "minutes": 0})
+dataset = BTgymDataset(filename='nvda_dataset.csv',
+                       parsing_params={
+                           "header": 0,
+                           "parse_dates": True,
+                           "index_col": 0,
+                           "sep": ",",
+                           "datetime": 0,
+                           "open": 1,
+                           "high": 2,
+                           "low": 3,
+                           "close": 4,
+                           "volume": 5,
+                           "openinterest": -1,
+                           "names": ["open", "high", "low", "close", "volume", "exdividend", "splitratio", "adjopen", "adjhigh", "adjlow", "adjclose", "adjvolume"],
+                           "timeframe": 1440
+                       },
+                       episode_duration={
+                           "days": 60,
+                           "hours": 0,
+                           "minutes": 0
+                       },
+                       time_gap={
+                           "days": 60,
+                           "hours": 0,
+                           "minutes": 0
+                       }
+                       )
 
 env = BTgymEnv(dataset=dataset, start_cash=1000)
 
